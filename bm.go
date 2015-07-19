525859b36e6bbd7fea3512ba7a12d5d38087bd22
Wim <wim@42.be>
2015-07-19 20:02:45 +0200

    Add the title of the URL and a timestamp

--- bm.go
+++ bm.go
@@ -9,19 +9,29 @@ import (
 	"net/http"
 	"strings"
 	"sync"
+	"time"
+
+	"golang.org/x/net/html"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-type bookMark struct {
+type Bmap struct {
 	sync.RWMutex
-	m map[string]string
+	m map[string]Bookmark
+}
+
+type Bookmark struct {
+	URL      string
+	Title    string
+	Category string
+	Modified time.Time
 }
 
 var flagFile string
 var flagPort string
 
-func (bm *bookMark) Save() {
+func (bm *Bmap) Save() {
 	bm.RLock()
 	res, err := json.Marshal(bm.m)
 	if err != nil {
@@ -31,9 +41,37 @@ func (bm *bookMark) Save() {
 	ioutil.WriteFile(flagFile, res, 0644)
 }
 
-func (bm *bookMark) Load() {
+func (bm *Bmap) Load() {
 	input, _ := ioutil.ReadFile(flagFile)
 	json.Unmarshal(input, &bm.m)
+}
+
+func getTitle(url string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20110814 Firefox/6.0")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	d := html.NewTokenizer(resp.Body)
+	for {
+		tokenType := d.Next()
+		if tokenType == html.ErrorToken {
+			return ""
+		}
+		token := d.Token()
+		if string(token.Data) == "title" {
+			d.Next()
+			return string(d.Text())
+		}
+	}
 }
 
 func init() {
@@ -43,7 +81,7 @@ func init() {
 }
 
 func main() {
-	bm := bookMark{m: make(map[string]string)}
+	bm := Bmap{m: make(map[string]Bookmark)}
 	bm.Load()
 	router := httprouter.New()
 	// these two statements below are actually the reason we need to use a 3rd party lib
@@ -54,19 +92,23 @@ func main() {
 		if strings.Contains(url, "favicon.ico") {
 			return
 		}
-		if strings.Contains(url, "http") || strings.Contains(url, ".") {
+		if strings.Contains(url, ".") {
 			bm.Lock()
-			bm.m[url] = "default" // implement categories, use default by now
+			if !strings.Contains(url, "http") {
+				url = "http://" + url
+			}
+			bm.m[url] = Bookmark{Title: getTitle(url), URL: url, Modified: time.Now(), Category: "default"}
 			bm.Unlock()
 			bm.Save()
 			http.Redirect(w, r, "/mybookmarks", 302)
 		}
 		if strings.Contains(url, "bookmarks") {
-			for k, _ := range bm.m {
+			for k, v := range bm.m {
 				//fmt.Fprintf(w, "<a href='%s'>%s</a> => %s<br>", k, k, v)
-				fmt.Fprintf(w, "<a href='%s'>%s</a><br>", k, k)
+				fmt.Fprintf(w, "<a href='%s'>%s</a> %s<br>", k, v.Title, v.Modified)
 			}
 		}
 	})
+	fmt.Println("starting webserver on " + flagPort)
 	log.Fatal(http.ListenAndServe(":"+flagPort, router))
 }
